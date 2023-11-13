891e054f699fdeeb285838f277f8be6cd72d8d04
Giulio Borzetta <giulioborz@gmail.com>
2023-11-13 13:54:18 +0100

    creazione counter

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let counter = document.querySelector("p");
+let i = 0;
+
+counter.innerHTML = "Il Counter si trova al numero:" + i;
+
+plus.addEventListener("click", increase);
+minus.addEventListener("click", decrease);
+
+function increase(plus){
+    while(plus){
+        i++;
+        console.log(i);
+        counter.innerHTML = "Il Counter si trova al numero:" + i;
+        return i;
+    }
+}
+
+function decrease(minus){
+    while(minus){
+        i--;
+        console.log(i);
+        counter.innerHTML = "Il Counter si trova al numero:" + i;
+        return i;
+    }
+}
+
+
